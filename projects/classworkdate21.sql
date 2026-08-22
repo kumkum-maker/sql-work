d099c5261c2b9c1da5e87620c270f8590ada525a
Kumkum Sharma <kumkumsharma1702@gmail.com>
2026-08-23 00:09:40 +0530

    Add files via upload

---
+++ projects/classworkdate21.sql
@@ -0,0 +1,29 @@
+show grants for kumkum;
+
+use jitu;
+
+insert into student values(3,'kumkum2',23);
+
+select * from student;
+
+
+update jitu.student 
+set student_name = 'kuldeep'
+where student_id = 1;
+
+
+delete from student
+where student_id = 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
